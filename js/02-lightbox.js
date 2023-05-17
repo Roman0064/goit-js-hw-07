a31b683cd5e07b436e53cf5706221496e1f9844a
Roman0064 <alee93474@gmail.com>
2023-05-17 23:38:45 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const bodyEl = document.querySelector("#root");
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryItemMarkup(item) {
+  const { preview, original, description } = item;
+
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+      </a>
+    </li>
+  `;
+}
+
+function renderGalleryItems(items) {
+  const galleryItemsMarkup = items.map(createGalleryItemMarkup).join('');
+
+  galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+}
+
+renderGalleryItems(galleryItems);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
