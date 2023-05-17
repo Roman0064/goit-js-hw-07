d186e55ee6d3b84442a029b6e3754f9fa1a07c3d
Roman0064 <alee93474@gmail.com>
2023-05-17 23:38:48 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,44 +2,6 @@ const bodyEl = document.querySelector("#root");
 
 import { galleryItems } from './gallery-items.js';
 
-// function createGalleryEl(items) {
-//     let galleryEl = document.querySelector(".gallery");
-//     let galleryElements = items.map(item =>{
-//         let itemEl = document.createElement("li");
-//         itemEl.classList.add("gallery-item");
-
-//         let imgEl = document.createElement("img");
-//         imgEl.classList.add("gallery__image");
-//         imgEl.src = item.preview;
-//         itemEl.appendChild(imgEl);
-
-//         let titleEl = document.createElement("p");
-//         titleEl.classList.add(".gallery__link");
-//         titleEl.innerText = item.description;
-//         itemEl.appendChild(titleEl);
-
-//         return itemEl;
-//     });
-//     galleryEl.append(...galleryElements);
-//     bodyEl.append(galleryEl);
-//     return galleryEl;
-// }
-
-// function galleryItemClick(event){
-//     console.log(event);
-// }
-
-// function bindGallery(galleryEl){
-//     galleryEl.addEventListener("click", galleryItemClick);
-// }
-
-// function initGallery(items){
-//     let galleryEl = createGalleryEl(items);
-//     bindGallery(galleryEl);
-// }
-
-// initGallery(galleryItems);
-
 const galleryContainer = document.querySelector('.gallery');
 
 function createGalleryItemMarkup(item) {
@@ -76,3 +38,5 @@ function openModal(imageURL) {
     const instance = basicLightbox.create(`<img src="${imageURL}" alt="Image">`);
     instance.show();
   }
+  
+console.log(galleryItems);
